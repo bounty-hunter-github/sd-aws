ed17eec88ccf575c05dc7c59126a0fb575f74642
ppaul <pritam.paul@verizonmedia.com>
2021-09-02 13:53:30 -0400

    deploy grey app

--- lambda/index.js
+++ lambda/index.js
@@ -1,17 +1,9 @@
 'use strict'
-const fs = require('fs');
 exports.handler = async (event) => {
-    const code = process.env.BG_COLOR;
-    //const image = fs.readFileSync('./Yahoo.png');
     const executor = process.env.EXECUTOR;
     const html = `
 <html>
-<style>
-    body {
-        background-image: url('./Yahoo.png');
-    }
-</style>
-<body background="./Yahoo.png">
+<body style="background-color:grey;">
     <h1>Screwdriver AWS Integration Lambda Demo!</h1>` +
     `<p style='background-color:tomato;'>This app is deployed using Screwdriver and AWS ${executor}.</p>
 </body>
