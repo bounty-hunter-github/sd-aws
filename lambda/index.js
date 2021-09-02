3b56d89b5f20f40f06682e1dc3d68ffc8ec079b0
ppaul <pritam.paul@verizonmedia.com>
2021-09-02 13:28:06 -0400

    feat: add brand image

--- lambda/index.js
+++ lambda/index.js
@@ -1,7 +1,8 @@
 'use strict'
-
+const fs = require('fs');
 exports.handler = async (event) => {
     const code = process.env.BG_COLOR;
+    //const image = fs.readFileSync('./Yahoo.png');
     const executor = process.env.EXECUTOR;
     const html = `
 <html>
@@ -10,7 +11,7 @@ exports.handler = async (event) => {
         background-image: url('./Yahoo.png');
     }
 </style>
-<body>
+<body background="./Yahoo.png">
     <h1>Screwdriver AWS Integration Lambda Demo!</h1>` +
     `<p style='background-color:tomato;'>This app is deployed using Screwdriver and AWS ${executor}.</p>
 </body>
