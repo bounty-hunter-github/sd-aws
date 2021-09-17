09d759859df8f6ee8a5e397e8f415918150c7ca1
ppaul <pritam.paul@verizonmedia.com>
2021-09-16 22:42:18 -0400

    feat: deploy a hapi app

--- lambda/index.js
+++ lambda/index.js
@@ -1,22 +1,76 @@
 'use strict'
-exports.handler = async (event) => {
-    const executor = process.env.EXECUTOR;
-    const html = `
-<html>
-<body style="background-color:#ffa700;">
-    <h1>Screwdriver AWS Integration Lambda Demo!</h1>` +
-    `<p style='background-color:#ffa700;font-size:30px'>This app is deployed using Screwdriver and AWS ${executor}.</p>
-</body>
-</html>
-    `
-    const response = {
-        statusCode: 200,
-        statusDescription: "200 OK",
-        isBase64Encoded: false,
-        headers: {
-            "Content-Type": "text/html"
-        },
-        body: html
+const api = require('./server');
+let server;
+
+const transformUrlPath = (event, options) => {
+    let url = event.path;
+  
+    // extract the stage from the url
+    if (options.stripStage) {
+      const currentStage = event.requestContext ? event.requestContext.stage : null;
+      if (currentStage) {
+        url = url.replace(`${currentStage}/`, '');
+      }
+    }
+  
+    // append qs params
+    const params = event.queryStringParameters;
+    if (params) {
+      const qs = Object.keys(params).map(key => `${key}=${params[key]}`);
+      if (qs.length > 0) {
+        url += `?${qs.join('&')}`;
+      }
+    }
+  
+    return url;
+  };
+
+const transformRequest = (event, options) => {
+    const opt = {
+      path: {
+        stripStage: false,
+      },
+      ...options,
     };
-    return response;
+  
+    return {
+      method: event.httpMethod,
+      url: transformUrlPath(event, opt.path),
+      payload: event.body,
+      headers: event.headers,
+      validate: false
+    };
+  };
+  
+  const transformResponse = response => {
+    const { statusCode } = response;
+  
+    const headers = {
+      ...response.headers,
+    };
+  
+    delete headers['content-encoding'];
+    delete headers['transfer-encoding'];
+  
+    let body = response.result;
+    if (typeof response.result !== 'string') {
+      body = JSON.stringify(body);
+    }
+  
+    return {
+      statusCode,
+      headers,
+      body
+    };
+  };
+
+exports.handler = async (event) => {
+    const request = transformRequest(event);
+    if (!server) {
+        server = await api.start();
+    }
+
+    const response = await server.inject(request);
+
+    return transformResponse(response);
 };
---
+++ lambda/server.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const Path = require('path');
+const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert')
+const Vision = require('@hapi/vision');
+
+const api = async () => {
+
+    const server = Hapi.server({
+        port: process.env.port || 4000,
+        host: 'localhost',
+        routes: {
+          files: {
+            relativeTo: Path.join(__dirname, 'public')
+          }
+        }
+      });
+    await server.register(Inert);
+    await server.register(Vision);
+
+    server.views({
+        engines: {
+            html: {
+                module: require('handlebars'),
+                compileMode: 'sync' 
+            }
+        },
+        relativeTo: __dirname,
+        path: 'public',
+        context: {
+            executor: process.env.EXECUTOR
+        }
+    });
+      
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: '.',
+                redirectToSlash: true,
+                index: true,
+            }
+        }
+    });
+
+    server.ext('onPreResponse', (request, h) => {
+        const response = request.response;
+        if (response.isBoom &&
+            response.output.statusCode === 404) {
+    
+            return h.file('404.html').code(404);
+        }
+    
+        return h.continue;
+    });
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+    
+    return server;
+};
+
+//api()
+module.exports = api
